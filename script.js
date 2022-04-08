3f38a4027117fb5bc88b883ee88eedc6dc55bf15
waifu pilot <q1233443@hotmail.com>
2022-04-08 12:50:06 -0700

    cleanUP function added

---
+++ script.js
@@ -0,0 +1,29 @@
+//Array of contacts
+let contactList = [
+  {
+    name: "Barry Allen",
+    phone: "+1 555 555-5555",
+    address: "123 front st, Unit #1, Dakota City",
+    email: "theflash@gmail.com",
+  },
+  {
+    name: "Beverly Crusher",
+    phone: "778-555-1234",
+    address: "101 Main St, Anytown, USA",
+    email: "chiefmedicalofficer@slack.example.com",
+  }, 
+  {
+    name: "Diana Prince",
+    phone: "123-867-5309",
+    address: "Warner Brothers Animation Lot",
+    email: "wonderwoman@doc.example.com",
+  }
+]
+
+//Index function 1
+function cleanUpindex() {
+var contactList = document.querySelector(".main");
+while (contactList.firstChild) {
+    contactList.removeChild(contactList.firstChild);
+}
+}
